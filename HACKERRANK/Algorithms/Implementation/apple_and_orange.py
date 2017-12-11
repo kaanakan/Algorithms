cf2abaef5bf8926c8a744c30dd8bdea9558e0106
Kaan Akan <34209020+kaanakan@users.noreply.github.com>
2017-12-11 10:27:59 +0300

    Create apple_and_orange.py

---
+++ HACKERRANK/Algorithms/Implementation/apple_and_orange.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import sys
+
+
+s,t = raw_input().strip().split(' ')
+s,t = [int(s),int(t)]
+a,b = raw_input().strip().split(' ')
+a,b = [int(a),int(b)]
+m,n = raw_input().strip().split(' ')
+m,n = [int(m),int(n)]
+apple = map(int,raw_input().strip().split(' '))
+orange = map(int,raw_input().strip().split(' '))
+print(len(list(filter(lambda x: a+x >= s and a+x <= t, apple))))
+print(len(list(filter(lambda x: b+x >= s and b+x <= t, orange))))
